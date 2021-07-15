361fd5daa0d1d46e40f3cb539bb7a9b70a2c2fae
sacarvy <sukhpreetben10@gmail.com>
2021-07-15 22:10:19 +0530

    Aded DropdownButton

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -1,15 +1,44 @@
 <template>
-  <div class="py-16">
+  <div class="py-4">
     <Switch
       v-model="enabled"
       :class="enabled ? 'bg-teal-900' : 'bg-teal-700'"
-      class="relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      class="
+        relative
+        inline-flex
+        flex-shrink-0
+        h-[38px]
+        w-[74px]
+        border-2 border-transparent
+        rounded-full
+        cursor-pointer
+        transition-colors
+        ease-in-out
+        duration-200
+        focus:outline-none
+        focus-visible:ring-2
+        focus-visible:ring-white
+        focus-visible:ring-opacity-75
+      "
     >
       <span class="sr-only">Use setting</span>
       <span
         aria-hidden="true"
         :class="enabled ? 'translate-x-9' : 'translate-x-0'"
-        class="pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200"
+        class="
+          pointer-events-none
+          inline-block
+          h-[34px]
+          w-[34px]
+          rounded-full
+          bg-white
+          shadow-lg
+          transform
+          ring-0
+          transition
+          ease-in-out
+          duration-200
+        "
       />
     </Switch>
   </div>
@@ -19,5 +48,4 @@
 import { ref } from 'vue'
 import { Switch } from '@headlessui/vue'
 const enabled = ref(false)
-
 </script>
--- src/pages/hi/[name].vue
+++ src/pages/hi/[name].vue
@@ -4,12 +4,17 @@ import { useI18n } from 'vue-i18n'
 import NavSector from '../../components/NavSector.vue'
 import Footer from '../../components/Footer.vue'
 import ToggleButton from '../../components/ToggleButton.vue'
+import DropdownButton from '../../components/DropdownButton.vue'
+import ModalButton from '../../components/ModalButton.vue'
 
 const ComponentList: Object = {
   footer: Footer,
   navsector: NavSector,
   togglebutton: ToggleButton,
+  dropdownbutton: DropdownButton,
+  modalbutton: ModalButton,
 }
+
 const props = defineProps<{ name: string }>()
 const router = useRouter()
 const { t } = useI18n()
@@ -18,13 +23,12 @@ const { t } = useI18n()
 <template>
   <div>
     Component Name : {{ props.name }}
-    <component :is="ComponentList[`${props.name}`]" />
+    <div class="p-10">
+      <component :is="ComponentList[`${props.name}`]" />
+    </div>
   </div>
   <!-- <div>{{ Footer.__defaults }}</div> -->
-  <button
-    class="btn-indigo btn"
-    @click="router.back()"
-  >
+  <button class="btn-indigo btn" @click="router.back()">
     {{ t('button.back') }}
   </button>
 </template>
